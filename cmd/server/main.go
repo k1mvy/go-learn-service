aaaaaf779b2b583d223ec76735f75754d04d70ba
k1mvy <link2vkim@gmail.com>
2025-12-03 23:43:25 +0300

    cron example

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-co-op/gocron/v2"
 )
 
 func main() {
@@ -14,8 +18,60 @@ func main() {
 		w.Write([]byte("welcome"))
 	})
 
-	err := http.ListenAndServe(":3000", r)
+	// cron scheduler
+	s, err := gocron.NewScheduler()
 	if err != nil {
 		panic(err)
 	}
+
+	_, err = addJobs(s)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//jobs, err := addJobs(s)
+	//for _, job := range jobs {
+	//	fmt.Printf("running job: %v", job.ID())
+	//}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	// http
+	go func() {
+		defer wg.Done()
+
+		fmt.Println("starting server")
+		err = http.ListenAndServe(":3000", r)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	// cron
+	go func() {
+		defer wg.Done()
+
+		fmt.Println("starting schedule")
+		s.Start()
+	}()
+
+	wg.Wait()
+}
+
+func addJobs(s gocron.Scheduler) ([]gocron.Job, error) {
+	j, err := s.NewJob(
+		gocron.DurationJob(
+			10*time.Second,
+		),
+		gocron.NewTask(
+			func() {
+				fmt.Println("job completed")
+			},
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return []gocron.Job{j}, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/k1mvy/go-learn-service
 
 go 1.25.4
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/go-co-op/gocron/v2 v2.18.2
+)
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jonboulle/clockwork v0.5.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+)
